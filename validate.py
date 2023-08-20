f809456e3b68b115ee00601d26df6e2db99e4704
Hermann Stamm-Wilbrandt <hermann@7600x.fritz.box>
2023-08-20 20:57:58 +0200

    delete superluous line

--- validate.py
+++ validate.py
@@ -8,7 +8,6 @@ from gmpy2 import mpz, log10  # pylint: disable=no-name-in-module
 from Colbert import C
 
 print("6 entries of the form [k,n,s,x,y], with p=k*2**n+1, s**2%p==p-1 and p==x**2+y**2")
-p = C[0][0] * 2 ** C[0][1] + 1
 for v in C:
     p = mpz(v[0]) * 2 ** mpz(v[1]) + 1
     print(
